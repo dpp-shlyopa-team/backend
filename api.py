71e3541b616a47ec97cd887802f718cc5e85a4f1
maksim-mshp <maksim-mshp@ya.ru>
2022-04-06 11:36:38 +0300

    API для каждого судна

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from database import Database
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from tqdm import tqdm
 
 app = FastAPI()
 app.add_middleware(
@@ -37,7 +38,7 @@ async def get_vessel(request: Request, vessel_id: int):
   db = Database()
   r1 = await db.fetchone("SELECT * FROM `dpp.shlyopa.db`.vessels WHERE MMSI = %s", [vessel_id])
   if (r1 == None):
-    raise HTTPException(404, 'Not found')
+    raise HTTPException(404, 'Not Found')
   r2 = await db.fetchall("SELECT datetime, latitude, longitude FROM `dpp.shlyopa.db`.stamps WHERE MMSI = %s", [vessel_id])
   a = {
     'MMSI': r1[0],
@@ -58,4 +59,36 @@ async def get_vessel(request: Request, vessel_id: int):
     "fields": fields,
     "data": r2,
     "main_data": a
-  })
+  }) 
+  
+@app.get("/api/vessels")
+async def get_vessel():
+  db = Database()
+  r1 = await db.fetchall("SELECT MMSI FROM `dpp.shlyopa.db`.stamps")
+  return r1
+
+@app.get("/api/vessel/{vessel_id}")
+async def get_vessel(vessel_id: int):
+  db = Database() 
+  r1 = await db.fetchone("SELECT * FROM `dpp.shlyopa.db`.vessels WHERE MMSI = %s", [vessel_id])
+  if (r1 == None):
+    raise HTTPException(404, 'Not found')
+  r2 = await db.fetchall("SELECT MMSI, VesselName, CallSign, Length, Width, Cargo, VesselType, TranscieverClass, Stamps FROM `dpp.shlyopa.db`.stamps WHERE MMSI = %s", [vessel_id])
+  a = {
+    'MMSI': r1[0],
+    'VesselName': r1[1],
+    'CallSign': r1[2],
+    'Length': r1[3],
+    'Width': r1[4],
+    'Cargo': r1[5],
+    'VesselType': r1[6],
+    'TranscieverClass': r1[7],
+    'Stamps': [None] * len(r2)
+  }
+  for i in range(len(r2)):
+    a['Stamps'][i] = {
+      'datetime': r2[i][1],
+      'latitude': r2[i][2],
+      'longitude': r2[i][3]
+    }
+  return a
