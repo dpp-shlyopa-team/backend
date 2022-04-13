1e54821adc7bc994d9a49ed897425f5ec8db4160
maksim-mshp <maksim-mshp@ya.ru>
2022-04-13 11:39:17 +0300

    обновлён colab

--- read_serial.py
+++ read_serial.py
@@ -1,8 +1,14 @@
 import serial
 import time
-ser = serial.Serial(port="COM7", baudrate=115200, bytesize=8, timeout=0.5, stopbits=serial.STOPBITS_ONE)
+ser = serial.Serial(port="COM8", baudrate=115200, bytesize=8, timeout=5, stopbits=serial.STOPBITS_ONE)
 while True:
-  s = ser.read(100)  # reading up to 100 bytes
-  # s = str(s)[2:-5]
-  print(s)
+  s = ser.read(1000)  # reading up to 100 bytes
+  # print(str(s)[2:-5].replace('\\r\\n', '\n'))
+  s = str(s)[2:-5].split('\\r\\n')
+  r = {}
+  for i in s:
+    i = i.split(': ')
+    if (len(i) > 1):
+      r[i[0]] = i[1]
+  print(r)
 ser.close()
